652779d7a683063239337a498e783d81731bb037
crezix <52518140+crezix@users.noreply.github.com>
2021-07-04 12:35:12 +0530

    && replace

--- LAB_02/q04.cpp
+++ LAB_02/q04.cpp
@@ -5,19 +5,19 @@ using namespace std;
 string getGrade(float score){
     string grade;
 
-    if(score>=0 && score<60){
+    if(score>=0 & score<60){
         grade = "Fail";
     }
-    else if(score>=60 && score<70){
+    else if(score>=60 & score<70){
         grade = "D";
     }
-    else if(score>=70 && score<80){
+    else if(score>=70 & score<80){
         grade = "C";
     }
-    else if(score>=80 && score<90){
+    else if(score>=80 & score<90){
         grade = "B";
     }
-    else if(score>=90 && score<100){
+    else if(score>=90 & score<100){
         grade = "A";
     }
     else{
